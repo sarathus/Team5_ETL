a996915690d8ba8a1ca5ff6273e6118487f3d19e
Thea Crockett <theagcrockett@gmail.com>
2019-08-17 12:41:20 -0700

    Add files via upload

---
+++ restaurant_ratings.sql
@@ -0,0 +1,15 @@
+CREATE TABLE "output_merge_placeid" (
+    "placeID" varchar(20)   NULL,
+    "latitude" varchar(20)   NOT NULL,
+	"longitude" varchar(20)   NOT NULL,
+	"name" varchar(100)   NULL,
+	"city" varchar(20)   NOT NULL,
+	"userID" varchar(20)   NOT NULL,
+	"rating" varchar (20)   NOT NULL,
+	"food_rating" varchar (20)   NOT NULL,
+	"service_rating" varchar (20)   NOT NULL
+	
+);
+DROP TABLE IF EXISTS output_merge_placeid;
+
+SELECT * from output_merge_placeid
